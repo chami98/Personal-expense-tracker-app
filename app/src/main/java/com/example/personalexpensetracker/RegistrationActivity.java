5792e04d0527aaee15cf9088d8caafaf4d22a3b4
chami98 <chamikara98@gmail.com>
2022-03-10 01:07:08 +0530

    Created Splash Screen with an animation , Creted Registration activity and login activiy

---
+++ app/src/main/java/com/example/personalexpensetracker/RegistrationActivity.java
@@ -0,0 +1,38 @@
+package com.example.personalexpensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button registerBtn;
+    private TextView registerQn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.registerBtn);
+        registerQn = findViewById(R.id.registerQn);
+
+        registerQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
