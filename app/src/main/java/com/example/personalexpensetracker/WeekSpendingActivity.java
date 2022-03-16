4590f8f720e39bd8c35c670616217767d1d8cb4d
chami98 <chamikara98@gmail.com>
2022-03-16 23:41:23 +0530

    Designed WeekSpendingActivity

---
+++ app/src/main/java/com/example/personalexpensetracker/WeekSpendingActivity.java
@@ -0,0 +1,14 @@
+package com.example.personalexpensetracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeekSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_spending);
+    }
+}
